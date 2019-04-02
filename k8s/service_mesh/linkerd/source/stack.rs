82f04526a10555430b72ae0026df88f2e7f6e316
ben-han-cn <ben.han.cn@gmail.com>
2019-04-02 15:39:43 +0800

    add more haskell related doc

--- k8s/service_mesh/linkerd/source/stack.rs
+++ k8s/service_mesh/linkerd/source/stack.rs
@@ -1,15 +1,27 @@
 pub trait Stack<T> {
+    type Error;
     type Value;
-    fn make(&self, target: T) -> Result<Self::Value, Self::Error>;
+    fn make(&self, target: &T) -> Result<Self::Value, Self::Error>;
+
+    fn push<U, L>(self, layer: L) -> L::Stack 
+    where 
+        L: Layer<U, T, Self>,
+        Self: Sized,
+    {
+        layer.bind(self)
+    }
 }
 //stack :: T -> Value
+//push :: Stack<T> -> Layer<U, T, Self> -> Stack<U>
 
 pub trait Layer<T, U, S: super::Stack<U>> {
     type Value;
+    type Error;
 
     fn bind(&self, next: S) -> Stack<T, Value = Self::Value, Error = Self::Error>;
 }
 //bind :: Stack<U> -> Stack<T>; change the param, but the return value of stack isn't changed
+//layer and stack is modeling a pipeline, each layer works on last layer's result
 
 
 pub trait Recognize<Request> {
